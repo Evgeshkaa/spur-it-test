4348ca0c78c7b4da7f7ac71a4b4f5483d3ecc7c3
Evgeshkaa <107510236+Evgeshkaa@users.noreply.github.com>
2022-08-21 15:24:02 +0300

    02

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,37 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+let collapsible = document.querySelector('.collapsible')
+let button = collapsible.querySelector('.collapsible__button');
+let content = collapsible.querySelector('.collapsible__content');
+let buttonVisible = button.querySelector('.collapsible__action.collapsible__action--visible');
+let buttonHidden = button.querySelector('.collapsible__action.collapsible__action--hidden');
+
+// Init
+content.hidden = true;
+buttonVisible.hidden = true;
+
+// Events
+buttonVisible.onclick = function () {
+    let animate = content.animate([
+        {opacity: 1},
+        {opacity: 0},
+    ], 300);
+
+    animate.onfinish = function() {
+        content.hidden = true;
+        buttonHidden.hidden = false;
+        buttonVisible.hidden = true;
+    };
+};
+
+buttonHidden.onclick = function () {
+    content.hidden = false;
+
+    let animate = content.animate([
+        {opacity: 0},
+        {opacity: 1},
+    ], 300);
+    
+    animate.onfinish = function() {
+        buttonHidden.hidden = true;
+        buttonVisible.hidden = false;
+    };
+};
